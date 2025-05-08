9db23e0a06b0c8b5c644ad9d234a47e29c39eb09
osmar vasquez <osmar@bedrock.io>
2025-05-08 00:41:01 -0600

    listend all interfaces

--- src/api/app.ts
+++ src/api/app.ts
@@ -64,7 +64,7 @@ class Server {
     }
 
     listen() {
-        this.app.listen(this.port, () => {
+        this.app.listen(this.port, '0.0.0.0', () => {
             console.log('server running in port: ', this.port);
         });
     }
